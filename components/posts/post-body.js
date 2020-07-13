9afeb46a8f5297d4f3fa314e471aef5ce636b6c4
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-07-13 21:34:44 +0700

    add component layout basic

---
+++ components/posts/post-body.js
@@ -0,0 +1,11 @@
+import { RichText } from 'prismic-reactjs'
+
+export default function PostBody({ content }) {
+  return (
+    <div className="max-w-2xl mx-auto">
+      <div className={markdownStyles['markdown']}>
+        <RichText render={content} />
+      </div>
+    </div>
+  )
+}
